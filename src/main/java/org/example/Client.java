9f95c07aa2c5b0e2d2fe529ccd0667b971c68e86
An0n1m0-us <an0n1m0us007.00.1@gmail.com>
2026-02-05 13:01:52 +0100

    a

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Client {
+
+    private string nome;
+    private string colore;
+    private Socket socket;
+
+
+  public int connetti(string nomeServer, int porta){
+
+  }
+
+  public void scrivi(){
+
+  }
+
+  public void leggi(){
+
+  }
+}
---
+++ src/main/java/org/example/Server.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Server {
+
+    private serverSocket serverSocket;
+    private socket clientSocket;
+    private int porta;
+
+    Server(int porta, serverSocket serverSocket, socket clientSocket){
+        this.porta=porta;
+    }
+
+    int connetti()
+}
